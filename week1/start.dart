d56e6d5af4d29b40d4374dbb3d03b282d5438d84
sinemsafak <safaksm14@gmail.com>
2024-02-20 19:11:45 +0300

    ekranda mesajın nasıl gösterildiği string ve int veri tipinin nasıl kullanıldığı gösterilmiştir.

--- week1/start.dart
+++ week1/start.dart
@@ -1,3 +1,26 @@
 void main(){
+  //bu kod ekrnda mesajımı gösterir
   print("Hello My First Project");
+
+
+  print("Hello"+" Veli");
+  print(2+3);
+
+  // veri Tipleri 
+  String verininAdi="verinin degeri";
+
+  String hello="Hello3";
+
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+  print(hello);
+
+  int money = 15;
+  print(money);
+
+  print(hello + money.toString()); //hello değişkeninin veri tipi string money değişkeninin veri tipi int olduğu için toplanmaz bu nedenle ya money değişkenine "toString"i ekleyeceğiz yada money değişkenini tırnak içine alıp başına $ işaretini ekleyeceğim 
+  
+
 }
